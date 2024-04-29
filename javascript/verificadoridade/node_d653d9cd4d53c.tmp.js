becbda954868427d4879ba3125b8ffe7a428aba9
UlissesNunes <ulissesnunesx@gmail.com>
2024-04-29 12:20:18 -0300

    att

---
+++ javascript/verificadoridade/node_d653d9cd4d53c.tmp.js
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="veridade.css">
+</head>
+<body>
+    <header>
+       <h1>Horário do dia</h1> 
+    </header>
+    <section> 
+       
+        <div id="container"> a mensagem vai aqui
+       </div>
+        <div id="fotos"> <img src="imagens/foto-manha.jpg.jpg" alt="foto">
+       </div>
+      </section>
+    <footer>
+        <p>&copy;Desenvolvido por Ulisses</p>
+    </footer>
+    <script>
+        window.alert('vou lutar pra dominar isso. Custe o')
+    </script>
+</body>
+</html>
