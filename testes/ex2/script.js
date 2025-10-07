6b1a0f11d68cee39fedac9386b7632c74be6b38c
UlissesNunes <ulissesnunesx@gmail.com>
2025-10-06 21:21:39 -0300

    ok

--- testes/ex2/script.js
+++
@@ -1,7 +0,0 @@
-function Verificar( {
-    var nome = document.querySelector('input#inome')
-    var sobrenome = document.querySelector ('input#isobrenome')
-    var res = document.querySelector ('div#res')
-    innerHTML = ´Seu nome completo é ${inome} ${isobrenome} ´
-   
-})
